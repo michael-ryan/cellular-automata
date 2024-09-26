57f6a86f76b1b907fe2d154c2de56aaa66f5d0dc
Michael Ryan <ryan.m1334@gmail.com>
2024-09-26 17:18:59 +0100

    documentation #major

--- models/transitionset.go
+++ models/transitionset.go
@@ -15,6 +15,22 @@ func NewTransitionSet() transitionSet {
 }
 
 // AddTransition adds a transition to this transitionSet. States are 0-indexed.
+// Rule is a func([][]uint, int int) bool that is called with:
+//   - c [][]uint: A 2D array that describes the all cell states, indexable as c[x][y] where c[0][0] is the bottom left cell
+//   - x: the cell in question's x coordinate
+//   - y: the cell in question's y coordinate
+//
+// It is encouraged to use the [At] function to safely index the cell states.
+//
+// For example, a transition rule where this cell turns from state 0 to state 1 if the cell to the right is in state 1 would look like this:
+//
+//	t.AddTransition(0, 1, func(c [][]uint, x int, y int) bool {
+//		target, err := At(c, x+1, y)
+//		if err != nil {
+//			return false
+//		}
+//		return target == 1
+//	})
 func (t *transitionSet) AddTransition(fromState, toState uint, rule predicate) {
 	maxIndex := len(*t) - 1
 	if maxIndex < int(fromState) {
